7ab02b54f454488e7fbabe337b3e2a633ba1ec7d
Abegenk <102190428+Abegenk@users.noreply.github.com>
2022-07-22 10:45:33 +0800

    Update settings.js

--- settings.js
+++ settings.js
@@ -54,7 +54,7 @@ global.fluming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutpu
 global.flarun = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=runner-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
 global.flasmurf = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=smurfs-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
 
-global.prefa = [','!','.','🐦','🐤','🗿']
+global.prefa = [' ','!','.','🐦','🐤','🗿']
 global.sp = '⭔'
 global.mess = {
     sukses: '🤗Done, Oke Desu~',
